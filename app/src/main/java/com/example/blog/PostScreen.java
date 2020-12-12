269596c4b8c2dd4af1d4bb10004e7e368f23e42b
DimaMamaev <dmamaev@my.com>
2020-12-12 20:53:21 +0200

    getting images from gallery

--- app/src/main/java/com/example/blog/PostScreen.java
+++ app/src/main/java/com/example/blog/PostScreen.java
@@ -1,8 +1,13 @@
 package com.example.blog;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,8 +20,11 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.StorageReference;
 
-public class PostScreen extends AppCompatActivity {
+import Utils.BlogApi;
 
+public class PostScreen extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int GALARY_CODE = 1;
     private Button saveBtn;
     private ProgressBar progressBar;
     private ImageView addPhoto;
@@ -24,6 +32,7 @@ public class PostScreen extends AppCompatActivity {
     private EditText postDesc;
     private TextView postUser;
     private TextView postDate;
+    private ImageView postImage;
 
     private String currentUserId;
     private String getCurrentUserName;
@@ -35,12 +44,15 @@ public class PostScreen extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private StorageReference storageReference;
     private CollectionReference collectionReference = db.collection("Posts");
+    private Uri imageUri;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_screen);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         progressBar = findViewById(R.id.progressBar_post);
         saveBtn = findViewById(R.id.button_post_create);
@@ -49,13 +61,68 @@ public class PostScreen extends AppCompatActivity {
         postDesc = findViewById(R.id.post_description);
         postUser = findViewById(R.id.post_username);
         postDate = findViewById(R.id.post_date);
+        postImage = findViewById(R.id.post_image);
+
+        saveBtn.setOnClickListener(this);
+
+        if (BlogApi.getInstance() != null) {
+            currentUserId = BlogApi.getInstance().getUserId();
+            getCurrentUserName = BlogApi.getInstance().getUsername();
+
+            postUser.setText(getCurrentUserName);
+        }
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                } else {
+
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_post_create:
+                break;
+            case R.id.add_post_image:
+                Intent toGalery = new Intent(Intent.ACTION_GET_CONTENT);
+                toGalery.setType("image/*");
+                startActivityForResult(toGalery, GALARY_CODE);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode, data);
+        if(requestCode == GALARY_CODE && resultCode == RESULT_OK) {
+            if(data != null) {
+                imageUri = data.getData();
+                postImage.setImageURI(imageUri);
+            }
+        }
+    }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
 
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
+
